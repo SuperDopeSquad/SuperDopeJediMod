7ff1ee2251683434280b7fab72110d3949dec2b5
mc <mc@madronahouse.com>
2015-05-28 08:08:20 -0700

    mc-laptop environment working!
    This is the official state of the bits where i can actually get things
    working on my laptop.  This will be the config i try to replicate on
    other machines.

---
+++ src/superDopeJediMod/BaseBlock.java
@@ -0,0 +1,27 @@
+package superDopeJediMod;
+
+import java.util.Random;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
+
+
+public class BaseBlock extends Block {
+
+	public BaseBlock(Material materialIn, String unlocalizedName) {
+		
+		super(materialIn);
+		
+		//this.setMaxStackSize(64);
+		this.setCreativeTab(CreativeTabs.tabBlock);
+		this.setUnlocalizedName(unlocalizedName);
+	}
+	
+	
+  
+    
+
+
+}
---
+++ src/superDopeJediMod/BrownSteelOre.java
@@ -0,0 +1,27 @@
+package superDopeJediMod;
+
+import java.util.Random;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
+
+
+public class BrownSteelOre extends BaseBlock {
+
+	protected BrownSteelOre(String unlocalizedName) {
+		
+		super(Material.rock, unlocalizedName);
+	}
+	
+	
+    public Item getItemDropped(int metadata, Random random, int fortune) {
+        
+    	return Item.getItemFromBlock(SuperDopeJediMod.brownSteel);
+    }
+  
+    
+
+
+}
