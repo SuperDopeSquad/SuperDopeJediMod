00870bc5aaddfd3d52631f0379e14ac34c27a34b
mc <michaelc@mac.com>
2015-08-10 16:21:52 -0700

    Revert "PowerCrystalOre-Textures"
    This reverts commit 9b5168dbcc560596ca6a4479e89c7b7f94ddb7ba.

--- src/superdopejedimod/PowerCrystalOre.java
+++ src/superdopejedimod/PowerCrystalOre.java
@@ -1,7 +1,6 @@
 package superdopesquad.superdopejedimod;
 
 import java.util.Random;
-
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -28,7 +27,7 @@ public class PowerCrystalOre extends BaseBlock {
 	
 	
 	public Item getItemDropped(int metadata, Random random, int fortune) {
-		
+        
 		Item obj;
 		
 		if (this.color == "Red") {
