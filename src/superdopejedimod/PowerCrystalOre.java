183aeca1530a96078756f6880ad9819fec595428
brynawesomehd321 <brynlharper@gmail.com>
2015-11-01 10:22:26 -0800

    powercrystalore changes

--- src/superdopejedimod/PowerCrystalOre.java
+++ src/superdopejedimod/PowerCrystalOre.java
@@ -69,10 +69,10 @@ public void registerRecipe() {
 	
 public void generateSurface(World world, Random random, int i, int j) {
 	
-	int maxVeinSize = 16;
+	int maxVeinSize = 4;
 	int minY = 0;
-	int maxY = 60;
-	int chancesPerChunk = 64; // A chunk is 16 blocks wide, 16 blocks long, and 256 blocks deep, which is 65,536 blocks total.
+	int maxY = 25;
+	int chancesPerChunk = 4; // A chunk is 16 blocks wide, 16 blocks long, and 256 blocks deep, which is 65,536 blocks total.
 	
 	SuperDopeJediMod.superDopeWorldGenerator.addOreSpawn(this, world, random, i, j, 16, 16, maxVeinSize, chancesPerChunk, minY, maxY); 
 }	
