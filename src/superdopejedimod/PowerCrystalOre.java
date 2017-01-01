76f40733cd02d53e2f953cb0fe789c7b603ecb7e
brynawesomehd321 <brynlharper@gmail.com>
2017-01-01 13:26:41 -0800

    Texture Changes, and also spawn rate changes for ore.
    i made the spawn rate for ruby ore and power crystal ore higher, and i
    fixed the texture for sith lord armor. I tried to fix the rotation on
    the lightsabers, but i couldnt fix it. If someone could take a look at
    that, that would be great!

--- src/superdopejedimod/PowerCrystalOre.java
+++ src/superdopejedimod/PowerCrystalOre.java
@@ -33,8 +33,8 @@ public class PowerCrystalOre extends BaseBlock {
 	}
 	
 	
-	public Item getItemDropped(int metadata, Random random, int fortune) {
-        
+	private Item getCorrectCrystal(String color) {
+		
 		Item obj = null;
 		
 		if (this.color == "Red") {
@@ -54,25 +54,55 @@ public class PowerCrystalOre extends BaseBlock {
 		}
 		
     	return obj;	
+	}
+	
+	
+	public Item getItemDropped(int metadata, Random random, int fortune) {
+        
+		return this.getCorrectCrystal(this.color);
+//		Item obj = null;
+//		
+//		if (this.color == "Red") {
+//			obj = SuperDopeJediMod.redPowerCrystal;
+//		}
+//		else if (this.color == "Green") {
+//			obj = SuperDopeJediMod.greenPowerCrystal;
+//		}
+//		else if (this.color == "Blue") {
+//			obj = SuperDopeJediMod.bluePowerCrystal;
+//		}
+//		else if (this.color == "Purple") {
+//			obj = SuperDopeJediMod.purplePowerCrystal;
+//		} 
+//		else {
+//			obj = SuperDopeJediMod.greenPowerCrystal;
+//		}
+//		
+//    	return obj;	
     }
 	
-
+	
 	public void registerRecipe() {
 		
+		Item obj = this.getCorrectCrystal(this.color);
+		if (obj != null) {
+			GameRegistry.addSmelting(this, new ItemStack(obj), 1.0F);
+		}
+		
 		// Smelting a PowerCrystalOre will create 1 PowerCrystal
-		GameRegistry.addSmelting(SuperDopeJediMod.redPowerCrystalOre, new ItemStack(SuperDopeJediMod.redPowerCrystal), 1.0F);
-		GameRegistry.addSmelting(SuperDopeJediMod.bluePowerCrystalOre, new ItemStack(SuperDopeJediMod.bluePowerCrystal), 1.0F);
-		GameRegistry.addSmelting(SuperDopeJediMod.greenPowerCrystalOre, new ItemStack(SuperDopeJediMod.greenPowerCrystal), 1.0F);
-		GameRegistry.addSmelting(SuperDopeJediMod.purplePowerCrystalOre, new ItemStack(SuperDopeJediMod.purplePowerCrystal), 1.0F);
+		//GameRegistry.addSmelting(SuperDopeJediMod.redPowerCrystalOre, new ItemStack(SuperDopeJediMod.redPowerCrystal), 1.0F);
+		////GameRegistry.addSmelting(SuperDopeJediMod.bluePowerCrystalOre, new ItemStack(SuperDopeJediMod.bluePowerCrystal), 1.0F);
+		//GameRegistry.addSmelting(SuperDopeJediMod.greenPowerCrystalOre, new ItemStack(SuperDopeJediMod.greenPowerCrystal), 1.0F);
+		//GameRegistry.addSmelting(SuperDopeJediMod.purplePowerCrystalOre, new ItemStack(SuperDopeJediMod.purplePowerCrystal), 1.0F);
 	}
 	
 	
 	public void generateSurface(World world, Random random, int i, int j) {
 	
-		int maxVeinSize = 3;
+		int maxVeinSize = 1;
 		int minY = 0;
 		int maxY = 12;
-		int chancesPerChunk = 1; // A chunk is 16 blocks wide, 16 blocks long, and 256 blocks deep, which is 65,536 blocks total.
+		int chancesPerChunk = 3; // A chunk is 16 blocks wide, 16 blocks long, and 256 blocks deep, which is 65,536 blocks total.
 	
 		SuperDopeJediMod.superDopeWorldGenerator.addOreSpawn(this, world, random, i, j, 16, 16, maxVeinSize, chancesPerChunk, minY, maxY); 
 	}	
--- src/superdopejedimod/RubyOre.java
+++ src/superdopejedimod/RubyOre.java
@@ -38,10 +38,10 @@ public class RubyOre extends BaseBlock{
 	
 	public void generateSurface(World world, Random random, int i, int j) {
 			
-			int maxVeinSize = 3;
+			int maxVeinSize = 2;
 			int minY = 0;
 			int maxY = 12;
-			int chancesPerChunk = 3; // A chunk is 16 blocks wide, 16 blocks long, and 256 blocks deep, which is 65,536 blocks total.
+			int chancesPerChunk = 5; // A chunk is 16 blocks wide, 16 blocks long, and 256 blocks deep, which is 65,536 blocks total.
 			
 			SuperDopeJediMod.superDopeWorldGenerator.addOreSpawn(this, world, random, i, j, 16, 16, maxVeinSize, chancesPerChunk, minY, maxY); 
 		}
