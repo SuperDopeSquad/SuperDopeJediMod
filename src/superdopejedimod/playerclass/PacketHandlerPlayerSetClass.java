2d0aa70135991d7fd2cdbc82854bead123bba510
mc <mc@madronahouse.com>
2017-02-18 08:35:17 -0800

    Clleanup: droid eggs, removing verbose logging

--- src/superdopejedimod/playerclass/PacketHandlerPlayerSetClass.java
+++ src/superdopejedimod/playerclass/PacketHandlerPlayerSetClass.java
@@ -22,24 +22,26 @@ public class PacketHandlerPlayerSetClass implements IMessageHandler<PacketPlayer
 	
 	@Override 
 	public IMessage onMessage(PacketPlayerSetClass message, MessageContext ctx) {
- 
-		//System.out.println("DEBUG: Message Received: " + message.getPlayerId() + ", " + message.getClassId());
+ 		
+		// MC-TODO: for some reason, when i log back in a second time, the event that i get doesn't have
+		// theWorld set correctly (it's null), so i bail on it.  But, my local state is already set correctly.
+		
 		
 		try {
 			int playerId = message.getPlayerId();
 			int classId = message.getClassId();
-			System.out.println("DEBUG: Message Contents: " + playerId + ", " + classId);
-			System.out.println("MINECRAFT:  " + (Minecraft.getMinecraft() != null));
-			System.out.println("WORLD: " + (Minecraft.getMinecraft().theWorld != null));
+			//System.out.println("DEBUG: Message Contents: " + playerId + ", " + classId);
+			//System.out.println("MINECRAFT:  " + (Minecraft.getMinecraft() != null));
+			//System.out.println("WORLD: " + (Minecraft.getMinecraft().theWorld != null));
 			
 			Entity entity = Minecraft.getMinecraft().theWorld.getEntityByID(playerId);
-			System.out.println("ENTITY:" + (entity != null));
+			//System.out.println("ENTITY:" + (entity != null));
 			//EntityPlayer player = (EntityPlayer) Minecraft.getMinecraft().theWorld.getEntityByID(playerId);
 			EntityPlayer player = (EntityPlayer) entity;
-			System.out.println("PLAYER:" + (player != null));
+			//System.out.println("PLAYER:" + (player != null));
 			boolean setSuccess = SuperDopeJediMod.classManager.setPlayerClassByClientId(player, classId);
 
-			System.out.println(setSuccess);
+			//System.out.println(setSuccess);
 		}
 		catch (Exception exception) {
 			System.out.println("ERROR: unpacking PacketPlayerSetClass: " + exception.getMessage());
