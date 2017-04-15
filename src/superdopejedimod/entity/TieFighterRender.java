8913dc9ba00df4deabb1b725be409b21a0a2692c
coolguybri <bri@coolguybri.com>
2017-04-15 11:41:06 -0700

    fix merge conflict
    baserender ->baserenderliving

--- src/superdopejedimod/entity/TieFighterRender.java
+++ src/superdopejedimod/entity/TieFighterRender.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import superdopesquad.superdopejedimod.SuperDopeJediMod;
 
 @SideOnly(Side.CLIENT)
-public class TieFighterRender extends BaseRender {
+public class TieFighterRender extends BaseRenderLiving {
 
 	public TieFighterRender(RenderManager renderManager, ModelBase par1ModelBase, float parShadowSize) {
         super(renderManager, par1ModelBase, parShadowSize, "tiefighter");   
